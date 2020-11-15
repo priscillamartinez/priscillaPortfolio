bb74c94774e50e9d9f22c5a5a9519c6375c603ea
priscillamartinez <priscillam54@gmail.com>
2020-11-15 10:03:28 -0600

    new files

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+// var i = 0;
+// var txt = 'Web Developer';
+// var speed = 50;
+
+// function typeWriter() {
+//     if (i < txt.length) {
+//         document.getElementById("info").innerHTML += txt.charAt(i);
+//         i++;
+//         setTimeout(typeWriter, speed);
+//     }
+// }
